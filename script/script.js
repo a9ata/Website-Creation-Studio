92de5f81b81b073a1b452f3b4cc3be79bfb8ed26
Агата <agata@MacBook-Pro-User.local>
2020-07-24 15:03:55 +0300

    Menu burger js

--- script/script.js
+++ script/script.js
@@ -44,3 +44,50 @@ $(function(){
 
     filterList.init();
 });
+
+$(document).ready(function(){
+    var headerHeight = $(".topheader").height();
+    $(window).on('scroll', {previousTop: 0},function(){
+        var currentTop = $(window).scrollTop();
+        if(currentTop < this.previousTop){//вверх прокрутка
+            if (currentTop > 0 && $(".topheader").hasClass('is-fixed')) {
+                
+            }else{//когда пользователь находится в самом верху
+                $(".topheader").removeClass('is-fixed')
+            }
+        }else{//крутит вниз
+            if (currentTop > headerHeight && !$(".topheader").hasClass('is-fixed')){
+                $(".topheader").addClass('is-fixed')
+            }
+        }
+            this.previousTop = currentTop;
+    });
+
+
+
+
+
+    $("primary-nav").on("click", "a", function(event){
+        event.preventDefault();
+        var id = $(this).attr("href"),
+            top = $(id).offset().top;
+        $('body, html').animate({scrollTop: top}, 1000);
+    });
+
+
+
+    $('.primary-nav-trigger').on("click", function(){
+    $(".menu-icon").toggleClass("is-clicked");
+     
+    if($(".primary-nav").hasClass("is-visible")){
+        // убираем
+        $(".primary-nav").removeClass("is-visible");
+        $("body").removeClass("overflow-hidden");
+    }else{
+        // добавляем
+     
+        $(".primary-nav").addClass("is-visible");
+        $("body").addClass("overflow-hidden");
+        } 
+    });
+});
